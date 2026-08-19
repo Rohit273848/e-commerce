c4bcd5e4ae55074ccf1bef8b19f68dc029398c80
Rohit Mahajan <rohitmahajan6942089@gmail.com>
2026-08-19 23:04:53 +0530

    navbar done

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from '../assets/logo.png'
+export default function Navbar() {
+    return (
+        <>
+            <nav className="flex bg-primary text-dark rounded-4xl justify-between items-center text-lg uppercase  px-6 py-2 font-sans 
+">
+                <div>
+                    <a href="">
+                        <h1 className="text-xl font-bold tracking-tight">MyShop</h1>
+                    </a>
+                </div>
+                <ul className="flex gap-9">
+                    <li><a href="#"className="transition-colors hover:text-white">Home</a></li>
+                    <li><a href="#"className="transition-colors hover:text-white">Products</a></li>
+                    <li><a href="#"className="transition-colors hover:text-white">About</a></li>
+                    <li><a href="#"className="transition-colors hover:text-white">Contact</a></li>
+                </ul>
+                <div className="flex gap-10" >
+                    <div className="flex gap-3 ">
+                        <button className="cursor-pointer rounded-full bg-dark px-5 py-2 text-sm font-medium text-white transition hover:opacity-90">Login</button>
+                        <button className="cursor-pointer rounded-full bg-dark px-5 py-2 text-sm font-medium text-white transition hover:opacity-90"> Register</button>
+                    </div>
+                    <a href="#" >
+                        <i className="ri-shopping-cart-line"></i>
+                    </a>
+                </div>
+
+            </nav>
+        </>
+    )
+}
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+    return (
+        <> <div className="bg-background min-h-screen p-5 ">
+            <Navbar />
+        </div>
+
+        </>
+    )
+}
